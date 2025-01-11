37901c9bd8adf0ed2e240dab64c0b68577b9ae95
Noreen Jenn Tajoda <noreenjenntajoda@gmail.com>
2025-01-11 14:32:21 +0800

    Enable cors and update response

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 def call_api():
-    url = ""
-    
+    url = "http://localhost:42429/scrape"
+
     try:
         response = requests.get(url)
-        
+
         if response.status_code == 200:
-            data = response.json()  
-            print(data)  
+            data = response.json()
+            return data
         else:
             print(f"API call failed with status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
+
 @app.get("/data")
-def read_item(location: str, complaint_type: str, timestamp: str):
-    call_api()
-    return {"location": location, "complaint": complaint_type, "timestamp": timestamp}
+def read_item():
+    data = call_api()
+    return data
